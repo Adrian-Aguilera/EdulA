1233e5f5274f52470f0a08e9940b0e0cbb33c1b6
Adrian-Aguilera <cruz74270@gmail.com>
2024-08-27 22:38:08 -0600

    testing: test response AV

--- Modules/AsistModel.py
+++ Modules/AsistModel.py
@@ -1,11 +1,20 @@
 from .GeneralModel import GeneralModel
 from EduAssistApp.models import AssistantCollection
 from asgiref.sync import async_to_sync, sync_to_async
+from Modules.GeneralModel import GeneralModel
 
 class AsistModel:
     async def responseAV(self, userMessage):
-        instancia = await sync_to_async(list)(AssistantCollection.objects.all())
-        nameCollection = instancia[0].nameCollection
-        
-        responseAV = nameCollection
-        return responseAV
+        try:
+            instancia = await sync_to_async(list)(AssistantCollection.objects.all())
+            nameCollection = instancia[0].nameCollection
+            EmbeddingsData = await GeneralModel._responseEmbedding(userMessage=userMessage, nameCollection=nameCollection)
+            responseGenerate = await GeneralModel._callGenerate(message_user=userMessage, contextEmbedding=EmbeddingsData)
+            if 'error' in EmbeddingsData:
+                return ({'error': EmbeddingsData['error']})
+            elif 'error' in responseGenerate:
+                return ({'error': responseGenerate['error']})
+            else:
+                return ({'response': responseGenerate})
+        except Exception as e:
+            return {"error": f"{str(e)}"}
