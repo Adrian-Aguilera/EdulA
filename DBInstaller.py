320cfb0c723da33682c379151b2f2b6896e3d3f9
Adrian-Aguilera <cruz74270@gmail.com>
2024-11-18 17:07:48 -0600

    fix: mensaje al crear la carpeta de la db

--- DBInstaller.py
+++ DBInstaller.py
@@ -8,9 +8,9 @@ Crear una carpeta para la base de datos
 def createDBFolder():
     try:
         os.mkdir("DB")
-        print("Carpeta creada")
+        print("Carpeta creada - >_<")
     except FileExistsError:
-        print("Carpeta ya existe")
+        print("Carpeta ya existe - X_X!")
 
 if __name__ == "__main__":
     createDBFolder()
