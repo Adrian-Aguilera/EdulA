6c1b7f1b4718001150ff6918cbd70114966ff8ee
Adrian-Aguilera <cruz74270@gmail.com>
2024-11-21 17:23:04 -0600

    fix: cambio en el codigo

--- EduEstudianteApp/views.py
+++ EduEstudianteApp/views.py
@@ -5,10 +5,9 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
-# Create your views here.
+
 class PerfilTokenObtainPairView(TokenObtainPairView):
     serializer_class = PerfilTokenObtainPairSerializer
-
 
 class LoginEstudiante(APIView):
     @api_view(['POST'])
