8475cc73ffdec1aab77ecaad8d1a73d5ceb22285
Adrian-Aguilera <cruz74270@gmail.com>
2024-11-21 17:53:22 -0600

    fix: mod pefiles desde el admin

--- EduEstudianteApp/admin.py
+++ EduEstudianteApp/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from .models import PerfilEstudiante
 # Register your models here.
 
-admin.site.register(PerfilEstudiante)
+class PerfilEstudianteAdmin(admin.ModelAdmin):
+    list_display = ('id','carnet')
+    search_fields  = ('id','carnet')
+
+admin.site.register(PerfilEstudiante, PerfilEstudianteAdmin)
