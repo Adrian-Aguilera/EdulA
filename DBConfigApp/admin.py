1e2144e80b063fe1cc0defe804662e69afd31306
Adrian-Aguilera <cruz74270@gmail.com>
2024-11-26 21:30:05 -0600

    fix: eliminacion de los antiguos mdelos de informacion

--- DBConfigApp/admin.py
+++ DBConfigApp/admin.py
@@ -1,9 +1,5 @@
 from django.contrib import admin
-from .models import DataGeneralChat, DataAsistenteChat, DocumentosRagGeneral, DocumentosRagAsistente
+from .models import DocumentosRagGeneral, DocumentosRagAsistente
 
-# Register your models here.
-
-admin.site.register(DataGeneralChat)
-admin.site.register(DataAsistenteChat)
 admin.site.register(DocumentosRagGeneral)
 admin.site.register(DocumentosRagAsistente)
