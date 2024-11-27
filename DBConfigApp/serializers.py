301005ca2def384c5a7daabf49ec9c4da416ffb4
Adrian-Aguilera <cruz74270@gmail.com>
2024-11-26 21:18:52 -0600

    feat: creacion del serializer para los documentos del asistente

--- DBConfigApp/serializers.py
+++ DBConfigApp/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
-from .models import DocumentosRagGeneral
+from .models import DocumentosRagGeneral, DocumentosRagAsistente
 
 class DocumentosRagGeneralSerializer(serializers.ModelSerializer):
     class Meta:
         model = DocumentosRagGeneral
+        fields = ['id', 'documento']
+
+class DocumentosRagAsistenteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DocumentosRagAsistente
         fields = ['id', 'documento']
